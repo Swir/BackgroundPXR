d2dca8a36db4d71cdff637cd3cade9d09b2a4f86
Jan Szczerbaty Uśmiechnięty <swirjestem@hotmail.com>
2026-09-10 13:55:48 +0200

    chore: bump BackgroundPXR to 0.3.4

--- backgroundpxr/__init__.py
+++ backgroundpxr/__init__.py
@@ -1,4 +1,4 @@
 """BackgroundPXR application package."""
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 APP_NAME = "BackgroundPXR"
-APP_SUBTITLE = "Power eXtreme Remover"
+APP_SUBTITLE = "AI Background Studio"
